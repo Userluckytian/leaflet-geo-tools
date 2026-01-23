34630afdba313a657a0ab74e31950c445c6326e4
tianzhuzhu <tianlukang123@163.com>
2026-01-23 09:58:09 +0800

    🐛 fix：修复高亮图层赋予linkLayerId无效的bug

--- src/topo/topo.ts
+++ src/topo/topo.ts
@@ -232,9 +232,9 @@ export class LeafletTopology {
     };
     const highlightLayer = L.geoJSON(layerGeom, {
       style: highlightStyle,
-      // ['linkLayerId' as any]: layer._leaflet_id, // 添加自定义属性
+      ['linkLayerId' as any]: layer._leaflet_id, // 添加自定义属性
     });
-    (highlightLayer as any).options.linkLayerId = layer._leaflet_id;
+    // (highlightLayer as any).options.linkLayerId = layer._leaflet_id; // 这样添加是不生效的！
     this.selectedLayers.push(highlightLayer);
     this.map && this.map.addLayer(highlightLayer);
   }
